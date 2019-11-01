86b56d12e05d0bdf1432958c1ff9c134589dae79
Andres Gutierrez <agutierrez.codes@gmail.com>
2019-11-01 17:16:19 +0100

    Mejorando formulario
    Se requere subir una imagen, ya que antes se podia subir cualquier archivo, para ello agregamos desde el controlador el requisito de que sea una imagen

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     {   
         $data = request()->validate([
             'caption' => 'required',
-            'image' => 'required'
+            'image' => ['required', 'image']
         ]);
         dd(request()->all());
     }
