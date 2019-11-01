c33af1aadb3a73da50313bb42d588936ce4bc6a4
Andres Gutierrez <agutierrez.codes@gmail.com>
2019-11-01 14:21:07 +0100

    Creando vistas para perfiles
    Se ha creado una ruta para cada uno de los usuarios, basandose en el ID, esos datos se han pasado por medio del controllador a la vista y ahora se muestra el nombre de usuario de manera dinamica

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
@@ -11,8 +12,11 @@ class ProfilesController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
+    public function index($user)
+    {   
+        $user = User::find($user);
+        return view('home', [
+            'user' => $user,
+        ]);
     }
 }
