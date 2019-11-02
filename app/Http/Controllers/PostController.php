667f237a5b74867ed478616f9dc433cbf2a9c1bd
Andres Gutierrez <agutierrez.codes@gmail.com>
2019-11-02 13:37:58 +0100

    Agregado recorte de imagen para Posts
    Haciendo uso de Intervetion se recorto la imagen para que fuera un cuadro  (1200x1200) y así no distorsione la diagramación de los posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PostController extends Controller
 {
@@ -23,6 +24,10 @@ class PostController extends Controller
         ]);
 
         $imagePath = request('image')->store('uploads', 'public');
+
+        $image = Image::make(public_path("storage/{$imagePath}"))->fit(1200, 1200);
+
+        $image->save();
 
         auth()->user()->posts()->create([
             'caption' => $data['caption'],
